2cd1cf56b967a1ba4ae287d19705c4c6e1bfbe93
akai <2780896955@qq.com>
2023-07-24 09:12:04 +0800

    add unroll

--- exact/exact/cuda_test/cuda-test.cu
+++ exact/exact/cuda_test/cuda-test.cu
@@ -62,6 +62,7 @@ Tensor test_rand_cuda(int N){
     return output;
 }
 #define LOW_MEM_DROPOUT_NUM_THREADS 512
+#define UNROLL 4
 template <typename scalar_t, int ADims, int BDims = ADims>
 __global__ void low_mem_dropout_forward_kernel(
     at::cuda::detail::TensorInfo<scalar_t, int64_t> data_info,
@@ -72,21 +73,27 @@ __global__ void low_mem_dropout_forward_kernel(
     uint64_t N,
     float p
 ){
-    const uint64_t id = blockIdx.x*blockDim.x + threadIdx.x;
+    const uint64_t base_id = blockIdx.x*blockDim.x*UNROLL + threadIdx.x;
     curandStatePhilox4_32_10_t state;
-    curand_init(seeds.first, id, seeds.second, &state);
-    float rand = curand_uniform(&state);
-    if(id < N){
-        const int64_t data_offset = at::cuda::detail::IndexToOffset<scalar_t, int64_t, 1>::get(id, data_info);
-        const int64_t output_offset = at::cuda::detail::IndexToOffset<scalar_t, int64_t, 1>::get(id, output_info);
-        const int64_t mask_offset = at::cuda::detail::IndexToOffset<bool, int64_t, 1>::get(id, mask_info);
-        // const int64_t rand_num_offset = at::cuda::detail::IndexToOffset<float, int64_t, 1>::get(id, rand_num_info);
-        // rand_num_info.data[rand_num_offset] = rand;
-        mask_info.data[mask_offset] = rand>p;
-        scalar_t data = data_info.data[data_offset];
-        scalar_t output = data*(rand>p);
-        output_info.data[output_offset] = output;
+    curand_init(seeds.first, base_id, seeds.second, &state);
+    float4 rand = curand_uniform4(&state);
+    float pinv = 1. / (1. - p);
+
+    for(int i=0; i<UNROLL; i++){
+        const uint64_t id = base_id+i*blockDim.x;
+        if(id < N){
+            const int64_t data_offset = at::cuda::detail::IndexToOffset<scalar_t, int64_t, ADims>::get(id, data_info);
+            const int64_t output_offset = at::cuda::detail::IndexToOffset<scalar_t, int64_t, BDims>::get(id, output_info);
+            const int64_t mask_offset = at::cuda::detail::IndexToOffset<bool, int64_t, BDims>::get(id, mask_info);
+            // const int64_t rand_num_offset = at::cuda::detail::IndexToOffset<float, int64_t, BDims>::get(id, rand_num_info);
+            // rand_num_info.data[rand_num_offset] = rand;
+            mask_info.data[mask_offset] = (&rand.x)[i]>p;
+            scalar_t data = data_info.data[data_offset];
+            scalar_t output = data*((&rand.x)[i]>p)*pinv;
+            output_info.data[output_offset] = output;
+        }
     }
+    
 }
 
 std::pair<Tensor, Tensor> low_mem_dropout_forward_cuda(Tensor data, float p){
@@ -94,20 +101,21 @@ std::pair<Tensor, Tensor> low_mem_dropout_forward_cuda(Tensor data, float p){
     for (size_t i = 0; i < data.dim(); ++i) {
         n_elements *= data.size(i);
     }
-    std::cout<<"n_elements:"<<n_elements<<std::endl;
+
     auto options = torch::TensorOptions().dtype(torch::kFloat32).device(data.device());
     Tensor output = torch::empty_like(data, options);
     options = torch::TensorOptions().dtype(torch::kBool).device(data.device());
     Tensor mask = torch::zeros_like(data, options);
+
     uint64_t block_size = LOW_MEM_DROPOUT_NUM_THREADS;
     unsigned int blocks_per_sm = at::cuda::getCurrentDeviceProperties()->maxThreadsPerMultiProcessor/block_size;
     // unsigned int n_blocks = at::cuda::getCurrentDeviceProperties()->multiProcessorCount * blocks_per_sm;
     dim3 dim_block(block_size);
-    dim3 grid((n_elements + block_size - 1) / block_size);
+    dim3 grid((n_elements + block_size*UNROLL - 1) / (block_size*UNROLL));
     // grid.x = std::min((unsigned int)at::cuda::getCurrentDeviceProperties()->multiProcessorCount * blocks_per_sm, grid.x);
     // uint64_t counter_offset = ((n_elements - 1)/(block_size*grid.x)+1);
     auto gen = at::check_generator<at::CUDAGeneratorImpl>(at::cuda::detail::getDefaultCUDAGenerator());
-    uint64_t seed = std::chrono::system_clock::now().time_since_epoch().count();
+    uint64_t seed = std::chrono::system_clock::now().time_since_epoch().count();//s, enough?
     gen->set_current_seed(seed);
     std::pair<uint64_t, uint64_t> rng_engine_inputs;
     {
